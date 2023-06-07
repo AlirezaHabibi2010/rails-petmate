1a481b146436655d56983159e451ba8aee47d8aa
Aaron Lorenzo Silva <aaronsilva95@outlook.es>
2023-06-07 12:37:16 +0200

    Updated seeds adding reviews, messages and bookmarks

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,6 +21,7 @@ def add_image(model, url)
     model.update(photos: downloaded_image)
   end
 end
+
 puts "Creating categories"
 categories = ["Dogs", "Cats", "Birds", "Fish", "Reptiles", "Small Mammals"]
 
@@ -61,11 +62,11 @@ descriptions = [
   "Adorable hamster that loves to run on the wheel."
 ]
 
-rand((7..10)).times.each do |i|
+rand(7..10).times.each do |i|
   puts "Pet number #{i}"
   random_pet_url = "https://source.unsplash.com/random/500x1000/?pet"
   pet = Pet.new(name: Faker::Creature::Dog.name, description: descriptions.sample, user_id: user_ids.sample, category_id: category_ids.sample)
-  rand((3..5)).times.each do |_|
+  rand(3..5).times.each do |_|
     add_image(pet, random_pet_url)
     pet.save!
     sleep(1.0)
@@ -75,7 +76,45 @@ end
 pet_ids = Pet.ids
 
 puts "Creating bookings"
-rand((10..15)).times.each do
+rand(10..15).times.each do
   booking = Booking.new(start_time: Faker::Time.between_dates(from: Date.today + 30, to: Date.today + 60, period: :day), end_time: Faker::Time.between_dates(from: Date.today + 61, to: Date.today + 90, period: :day), pet_id: pet_ids.sample, user_id: user_ids.sample)
-  puts booking.save
+  booking.save!
+end
+
+puts "Creating reviews"
+booking_ids = Booking.ids
+
+rand(50..100).times do
+  booking_id = booking_ids.sample
+  booking = Booking.find(booking_id)
+  # next if booking.reviews
+
+  review = Review.new(
+    content: Faker::Lorem.paragraph(sentence_count: 2, supplemental: true),
+    rating: rand(0..5),
+    booking_id: booking_id
+  )
+  review.save!
+end
+
+puts "Creating messages"
+rand(10..15).times do
+  booking_id = booking_ids.sample
+  user_id = user_ids.sample
+
+  message = Message.new(
+    content: Faker::Lorem.sentence(word_count: 5, supplemental: true),
+    booking_id: booking_id,
+    user_id: user_id
+  )
+  message.save!
+end
+
+puts "Creating bookmarks"
+rand(5..10).times do
+  user_id = user_ids.sample
+  pet_id = pet_ids.sample
+
+  bookmark = Bookmark.new(user_id: user_id, pet_id: pet_id)
+  bookmark.save!
 end
