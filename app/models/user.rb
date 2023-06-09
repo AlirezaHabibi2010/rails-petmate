220184eb3163bf29e848b69210a4133a027debe1
Alireza Habibi <habibi.alireza2010@gmail.com>
2023-06-09 15:36:08 +0200

    redis heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :photo
-  has_many :pets, through: :bookmarks
+  has_many :pets
   has_many :bookings
   has_many :messages
   has_many :bookmarks, dependent: :destroy
